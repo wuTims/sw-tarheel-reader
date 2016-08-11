f0b9010b27bea591ab12bc7eff9afd165e5e73ec
Gary Bishop <gary.bishop.unc@gmail.com>
2016-08-11 11:57:20 -0400

    change path

--- components/Html.js
+++ components/Html.js
@@ -9,12 +9,11 @@ class Html extends React.Component {
                 <meta charSet="utf-8" />
                 <title>{this.props.context.getStore(ApplicationStore).getPageTitle()}</title>
                 <meta name="viewport" content="width=device-width, user-scalable=no" />
-                <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css" />
             </head>
             <body>
                 <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
                 <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-                <script src={'/public/js/' + this.props.clientFile}></script>
+                <script src={'/public/' + this.props.clientFile}></script>
             </body>
             </html>
         );
