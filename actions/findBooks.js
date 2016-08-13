fedb8e8db09ebe604ab092adc7f79a6d40b4cba0
Gary Bishop <gary.bishop.unc@gmail.com>
2016-08-13 07:48:00 -0400

    fetching from THR

--- actions/findBooks.js
+++ actions/findBooks.js
@@ -1,6 +1,5 @@
 module.exports = function (context, payload, callback) {
     context.service.read('books', {}, {}, function(err, books) {
-        console.log('serice.read resp', err, books);
         if (err) {
             callback(err);
         } else {
--- services/findService.js
+++ services/findService.js
@@ -1,9 +1,16 @@
 var books = require('../dummy/books.json');
 
+var fetch = require('node-fetch');
+
 module.exports = {
     name: "books",
     read: function(req, resource, params, config, callback) {
-        // We would do some kind of database call here.
-        callback(null, books);
+        // fetch books from THR
+        // how to pass parameters?
+        fetch('http://tarheelreader.org/find/?json=1')
+            .then(res => res.json())
+            .then(json => {
+                callback(null, json.books);
+            });
     }
 }
